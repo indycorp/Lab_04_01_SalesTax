ca38d441a7a2ba2967099b028db9cf82a3c1eabe
indycorp <ikandindy@gmail.com>
2026-09-21 12:59:53 -0400

    Init

---
+++ src/Lab_04_01_salesTax.java
@@ -0,0 +1,32 @@
+public class Lab_04_01_salesTax {}
+    static void main() {
+    double price = 49.99;
+    double taxRate =0.05;
+    double salesTax = price * taxRate;
+    double Total = price + salesTax;
+
+    IO.println("Price: $" + price);
+    IO.println("sales tax (5%): $" + salesTax);
+    IO.println("your total price plus tax is: " + Total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
